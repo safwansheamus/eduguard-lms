6690e4a5cd0d2243c3f6feea4973a2b3164f6630
safwansheamus <cruelpennywise@gmail.com>
2023-12-31 01:52:16 +0700

    fix-error-chapter-functionality-and-edit-chapter-page

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-form.tsx
@@ -4,7 +4,7 @@ import * as z from "zod";
 import axios from "axios";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import { PlusCircle } from "lucide-react";
+import { Loader2, PlusCircle } from "lucide-react";
 import { useState } from "react";
 import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
@@ -19,11 +19,11 @@ import {
 } from "@/components/ui/form";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
-import { Textarea } from "@/components/ui/textarea";
 import { Input } from "@/components/ui/input";
+import { ChapterList } from "./chapters-list";
 
 interface ChaptersFormProps {
-  initialData: Course & {chapters: Chapter[]};
+  initialData: Course & { chapters: Chapter[] };
   courseId: string;
 };
 
@@ -35,7 +35,7 @@ export const ChaptersForm = ({
   initialData,
   courseId
 }: ChaptersFormProps) => {
-  const [isCreating, setIsCreating] = useState(false);
+  const [isCreating, setIsCreating] = useState(false)
   const [isUpdating, setIsUpdating] = useState(false);
 
   const toggleCreating = () => {
@@ -47,7 +47,7 @@ export const ChaptersForm = ({
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      title: "",
+      title: ""
     },
   });
 
@@ -64,17 +64,43 @@ export const ChaptersForm = ({
     }
   }
 
+  const onReorder = async (updateData: { id: string; position: number }[]) => {
+    try {
+      setIsUpdating(true);
+
+      await axios.put(`/api/courses/${courseId}/chapters/reorder` , {
+        list: updateData
+      });
+      toast.success("Chapters reordered");
+      router.refresh();
+    } catch {
+      toast.error("Something went wrong");
+    } finally {
+      setIsUpdating(false);
+    }
+  }
+
+  const onEdit = (id: string) => {
+    router.push(`/teacher/courses/${courseId}/chapters/${id}`);
+  }
+
+
   return (
-    <div className="mt-6 border bg-slate-100 rounded-md p-4">
+    <div className="relative mt-6 border bg-slate-100 rounded-md p-4">
+      {isUpdating && (
+        <div className="absolute h-full w-full bg-slate-500/20 top-0 right-0 rounded-m flex items-center justify-center">
+          <Loader2 className="animate-spin h-6 w-6 text-sky-700" />
+        </div>
+      )}
       <div className="font-medium flex items-center justify-between">
-        Course Chapters
+        Course chapters
         <Button onClick={toggleCreating} variant="ghost">
           {isCreating ? (
             <>Cancel</>
           ) : (
             <>
               <PlusCircle className="h-4 w-4 mr-2" />
-              Add a Chapter
+              Add a chapter
             </>
           )}
         </Button>
@@ -93,7 +119,7 @@ export const ChaptersForm = ({
                   <FormControl>
                     <Input
                       disabled={isSubmitting}
-                      placeholder="e.g. 'Introduction to the course'"
+                      placeholder="Introduction to the course"
                       {...field}
                     />
                   </FormControl>
@@ -116,7 +142,11 @@ export const ChaptersForm = ({
           !initialData.chapters.length && "text-slate-500 italic"
         )}>
           {!initialData.chapters.length && "No Chapters"}
-          {/* TODO: add a list of chapters */}
+          <ChapterList 
+            onEdit={onEdit}
+            onReorder={onReorder}
+            items={initialData.chapters || []}
+          />
         </div>
       )}
       {!isCreating && (
