53c64f63e0d12bfd68df9334709779a2d5781696
safwansheamus <cruelpennywise@gmail.com>
2024-01-03 03:54:44 +0700

    build-stripe-not-working-yet

--- app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-enroll-button.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-enroll-button.tsx
@@ -1,5 +1,9 @@
 "use client"
 
+import axios from "axios";
+import { useState } from "react";
+import toast from "react-hot-toast";
+
 import { Button } from "@/components/ui/button"
 import { formatPrice } from "@/lib/format";
 
@@ -12,6 +16,22 @@ export const CourseEnrollButton = ({
     price,
     courseId,
 }: CourseEnrollButtonProps) => {
+    const [isLoading, setIsLoading] = useState(false);
+
+    const onClick = async () => {
+        try {
+          setIsLoading(true);
+    
+          const response = await axios.post(`/api/courses/${courseId}/checkout`)
+    
+          window.location.assign(response.data.url);
+        } catch {
+          toast.error("Something went wrong");
+        } finally {
+          setIsLoading(false);
+        }
+      }
+
     return ( 
         <Button className="w-full md:w-auto">
             Enroll for {formatPrice(price)}
