a47a09320a25e1f0f98046dc49d9561f4c304c92
safwansheamus <cruelpennywise@gmail.com>
2024-01-10 14:43:56 +0700

    update

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="h-screen flex" style={{ backgroundImage: 'url("https://tse3.mm.bing.net/th?id=OIP.QeIei0obzoLOyKr7SE-QwgHaEK&pid=Api&P=0&h=180")', backgroundSize: 'cover', backgroundPosition: 'center' }}>
+    <div className="h-screen flex" style={{ backgroundImage: 'url("https://raw.githubusercontent.com/safwansheamus/eduguard-lms/main/public/bg.png")', backgroundSize: 'cover', backgroundPosition: 'center' }}>
       <div className="w-1/2 p-8 flex flex-col justify-center items-center bg-opacity-75">
         <h2 className="text-3xl font-bold mb-4">Selamat Datang!</h2>
         <p className="text-lg text-gray-700 text-center">Nikmati pengalaman istimewa dengan layanan kami.</p>
