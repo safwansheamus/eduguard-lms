c1b811174643b446b2fe0a7400edcb7843b36253
safwansheamus <cruelpennywise@gmail.com>
2024-01-10 19:17:11 +0700

    update-bg-logo

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="h-screen flex" style={{ backgroundImage: 'url("https://raw.githubusercontent.com/safwansheamus/eduguard-lms/main/public/bgupdatde.png")', backgroundSize: 'cover', backgroundPosition: 'center' }}>
+    <div className="h-screen flex overflow-hidden" style={{ backgroundImage: 'url("https://raw.githubusercontent.com/safwansheamus/eduguard-lms/main/public/kambing.png")', backgroundSize: 'cover', backgroundPosition: 'center' }}>
       <div className="w-1/2 p-8 flex flex-col justify-center items-center bg-opacity-75">
         <br></br>
-        <h2 className="text-3xl font-bold mb-4">EduGuard-LMS</h2>
-        <p className="text-lg text-gray-700 text-center">Nikmati pengalaman istimewa dengan layanan kami.</p>
+        <h2 className="text-5xl font-bold mb-4">EduGuard-LMS</h2>
+        <p className="text-lg text-gray-700 text-center font-semibold">Tingkatkan Keamanan Digital Anda dengan Pengetahuan Berkualitas Tinggi! Dapatkan Akses Penuh ke Pembelajaran Cybersecurity dengan Harga yang Terjangkau! Kami yakin bahwa perlindungan online yang handal tak harus memberatkan dompet Anda.</p>
       </div>
 
-      <div className="w-90 flex items-center justify-end">
-        <div className="p-8 bg-transparent">
+      <div className="w-90 flex items-center justify-end overflow-hidden">
+        <div className="p-80 bg-transparent">
           {children}
         </div>
       </div>
